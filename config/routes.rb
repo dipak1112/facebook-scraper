defb2f7f2fd6efd38ea414b539ae95d3638a0f74
nikhil <nikhil@dipak.cs>
2014-01-08 15:47:24 +0530

    fixing issue

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,12 @@ FacebookScraper::Application.routes.draw do
       get 'facebook_friends'
       get 'facebook_groups'
       get 'profile'
+      get 'my_albums'
     end
   end
   match '/users/facebook_group_member(/:group_id)', to: 'users#facebook_group_member', via: [:get], :as => "facebook_group_member_users"
   match '/users/profile(/:fbid)', to: 'users#profile', via: [:get], :as => "my_facebook_profile"
+  match '/users/my-album-photos(/:album_id)', to: 'users#my_album_photos', via: [:get], :as => "my_album_photos"
 
   match '/dashboard', to: 'home#dashboard', via: [:get]
   match 'auth/:provider/callback', to: 'sessions#create' , via: [:get, :post]
